617252e8782a3b0f1298292affefaad63b17f49a
sivaram <ramsivak9@gmail.com>
2023-11-22 00:21:06 +0530

    merged to main

---
+++ terraform_hcl/AL2023/data.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "amzn2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+data "aws_ami" "amzn3" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
---
+++ terraform_hcl/e-sg-vpc-lb/modules/ec2/data.tf
@@ -0,0 +1,27 @@
+data "aws_ami" "amzn2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+data "aws_ami" "amzn-linux-2023-ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+}
+
+data "aws_availability_zones" "available" {
+    state = "available"
+}
